dfed462e1042ffa4999b2f72816090477ba3a497
casa <casa@L124993.uao.edu.co>
2020-05-08 21:08:11 -0500

    Aplicacion finalizada

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 Route::get('/register', function(){
 	return redirect('/');
 });
