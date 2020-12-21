3ddbfe288b68c1507273567409adcce5113f9a44
aehenao <andrestivenhenao@gmail.com>
2020-12-21 07:37:38 -0500

    Ajustes

--- resources/views/reedirectPage.blade.php
+++ resources/views/reedirectPage.blade.php
@@ -25,7 +25,7 @@
             -webkit-background-size: cover;
             -moz-background-size: cover;
             -o-background-size: cover;
-            background-size: cover;
+            background-size: cover !important;
         }
 
         .gradient {
@@ -86,7 +86,7 @@
     </style>
 </head>
 
-<body style=" background: url({{ asset($link->imagen) }});">
+<body style=" background: url({{ asset($link->imagen) }}) ;">
     <span id="data" time="{{ $link->tiempo_espera }}" style="display: none;" destino="{{ $link->destino }} "></span>
 
     <div class="wrapper">
